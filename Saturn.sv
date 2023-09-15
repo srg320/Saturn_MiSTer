414b2547e7cb59f5c92cf1b0af4e15c5077e10cc
Sergey Dvodnenko <dvodnenkos@yandex.ua>
2023-09-15 10:41:04 +0300

    Add missing port

--- Saturn.sv
+++ Saturn.sv
@@ -47,6 +47,7 @@ module emu
 	output        VGA_F1,
 	output [1:0]  VGA_SL,
 	output        VGA_SCALER, // Force VGA scaler
+	output        VGA_DISABLE, // analog out is off 
 
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
@@ -221,6 +222,7 @@ module emu
 	assign AUDIO_S = 1;
 	assign AUDIO_MIX = 0;
 	assign HDMI_FREEZE = 0;
+	assign VGA_DISABLE = 0;
 	
 	assign LED_DISK  = 0;
 	assign LED_POWER = 0;
